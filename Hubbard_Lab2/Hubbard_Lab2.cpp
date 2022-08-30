1ff9aec4b32c7797b421b439c436340b03c1d151
Grant Hubbard <gdhubbar@purdue.edu>
2022-08-30 13:10:17 -0400

    Lab 2

--- Hubbard_Lab2/Hubbard_Lab2.cpp
+++ Hubbard_Lab2/Hubbard_Lab2.cpp
@@ -1,20 +1,30 @@
 // Hubbard_Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+# include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	float A;
+	float B;
+	float X;
+
+	cout << "Let's solve an equation for Ax + B =0 " << endl;
+
+
+	cout << "Please enter a value for A: ";
+	cin >> A;
+	cout << "Please enter a value for B: ";
+	cin >> B;
+
+	cout << "Okay, so A=" << A << endl;
+	cout << "And B=" << B << endl;
+	
+	cout << "Lets solve this equation for x!" << endl;
+
+	X = (0 - B) / A;
+	cout << "Solved it!" << endl;
+	cout << "X = " << X << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
